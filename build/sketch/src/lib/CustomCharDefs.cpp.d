78661da0e9a7485f1d3b3faf6dde16c321565247
jbotello <jlnbotello@gmail.com>
2024-04-29 02:06:19 +0200

    remove build from versioning

--- build/sketch/src/lib/CustomCharDefs.cpp.d
+++
@@ -1,5 +0,0 @@
-C:\Users\jlnbo\Repos\esp8266\app01\build\sketch\src\lib\CustomCharDefs.cpp.o: \
- C:\Users\jlnbo\Repos\esp8266\app01\build\sketch\src\lib\CustomCharDefs.cpp \
- C:\Users\jlnbo\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\CommonHFile.h \
- C:\Users\jlnbo\Repos\esp8266\app01\build\core\app01.ino.globals.h \
- C:\Users\jlnbo\Repos\esp8266\app01\build\sketch\src\lib\CustomCharDefs.h
--- build/sketch/src/models/TimeModel.cpp
+++
@@ -1,2 +0,0 @@
-#line 1 "C:\\Users\\jlnbo\\Repos\\esp8266\\app01\\src\\models\\TimeModel.cpp"
-#include "TimeModel.h"
