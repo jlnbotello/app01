fbd603b16551e2ef8e4878cb33f71db2effab7bd
jbotello <jlnbotello@gmail.com>
2024-05-08 00:24:52 +0200

    feat: add AccessControl

---
+++ src/models/AccessControl.h
@@ -0,0 +1,133 @@
+
+#ifndef __ACCESS_CONTROL_H__
+#define __ACCESS_CONTROL_H__
+
+#include <EEPROM.h>
+
+#define NAME_LENGTH 16
+
+#define ACCESS_ENTRIES 20
+#define EEPROM_SIZE ACCESS_ENTRIES * sizeof(AccessEntry)
+
+#define NULL_IDENTIFIER 0
+#define INVALID_INDEX 0xFF
+
+static const char * const DEFAULT_NAME = "Unknown";
+
+typedef struct {
+    uint16_t identifier;
+    char name[NAME_LENGTH+1];
+} AccessEntry;
+
+class AccessControl {
+private:
+    AccessEntry entries[ACCESS_ENTRIES];
+public:
+    AccessControl()
+    {
+        EEPROM.begin(EEPROM_SIZE);
+        
+        for (int i = 0; i < ACCESS_ENTRIES; i++)
+        {
+            entries[i].identifier = NULL_IDENTIFIER;
+            strncpy(entries[i].name, DEFAULT_NAME, NAME_LENGTH);
+            entries[i].name[NAME_LENGTH] = '\0';
+            restoreFromEEPROM(i);
+        }
+    }
+
+    bool addEntry(uint16_t identifier, const char *name){
+
+        uint8_t index = findEntry(identifier);
+
+        if(index != INVALID_INDEX)
+        {
+            return false; // duplicated entry
+        }
+
+        index = findEntry(NULL_IDENTIFIER);
+
+        if(index == INVALID_INDEX)
+        {
+            return false; // no space left
+        }
+
+        entries[index].identifier = identifier;
+        strncpy(entries[index].name, name, NAME_LENGTH);
+
+        saveToEEPROM(index);
+
+        return true;
+    }
+
+    void removeEntry(uint16_t identifier)
+    {
+        uint8_t index = findEntry(identifier);
+
+        if (index != INVALID_INDEX)
+        {
+            entries[index].identifier = NULL_IDENTIFIER;
+            saveToEEPROM(index);
+        }
+    }
+
+    void saveToEEPROM(uint8_t index)
+    {
+        EEPROM.put(index * sizeof(AccessEntry), entries[index]);
+        EEPROM.commit();
+    }
+
+    void restoreFromEEPROM(uint8_t index)
+    {
+        EEPROM.get(index * sizeof(AccessEntry), entries[index]);
+    }
+
+    bool checkAccess(uint16_t identifier)
+    {
+        return findEntry(identifier) != INVALID_INDEX;
+    }
+
+    uint8_t findEntry(uint16_t identifier)
+    {
+        for (int i = 0; i < ACCESS_ENTRIES; i++)
+        {
+            if(entries[i].identifier == identifier)
+            {
+                return i;
+            }
+        }
+        return INVALID_INDEX;
+    }
+
+    void printEntries()
+    {
+        for (int i = 0; i < ACCESS_ENTRIES; i++)
+        {
+            if(entries[i].identifier == NULL_IDENTIFIER)
+            {
+                continue;
+            }
+
+            Serial.print("Entry ");
+            Serial.print(i);
+            Serial.print(": ");
+            Serial.print(entries[i].identifier);
+            Serial.print(" ");
+            Serial.println(entries[i].name);
+        }
+    }
+
+    void clearEntries()
+    {
+        for (int i = 0; i < ACCESS_ENTRIES; i++)
+        {
+            entries[i].identifier = NULL_IDENTIFIER;
+            strncpy(entries[i].name, DEFAULT_NAME, NAME_LENGTH);
+            EEPROM.put(i * sizeof(AccessEntry), entries[i]);
+        }
+        
+        EEPROM.commit();
+    }
+};
+
+#endif /* __ACCESS_CONTROL_H__ */
