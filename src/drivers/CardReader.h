631ef3cf5712eb0de68344ec25b01dfa06d16b98
jbotello <jlnbotello@gmail.com>
2024-05-09 17:25:07 +0200

    clean: CardReader

--- src/drivers/CardReader.h
+++ src/drivers/CardReader.h
@@ -1,17 +1,14 @@
+#ifndef __CARDREADER_H__
+#define __CARDREADER_H__
+
 #include <SPI.h>
 #include <MFRC522.h>
-
-#include "AccessControl.h"
 #include <String>
-
-#define DELETE_TAG  "A9 CB 7A 48"
-#define ADD_TAG     "36 6C E8 F7"
-#define PRINT_TAG   "E9 A3 73 48"
 
 class CardReader
 {
 public:
-    CardReader(MFRC522 & instance) : mfrc522(instance), accessControl(*(new AccessControl()))
+    CardReader(MFRC522 & instance) : mfrc522(instance)
     {
    
     }
@@ -60,27 +57,7 @@ public:
     {
         if (isNewCardPresent())
         {         
-            if(isUID(DELETE_TAG) && status != 1)
-            {
-                Serial.println("Delete Tag");
-                status = 1;
-                accessControl.clearEntries();
-            }
-
-            if(isUID(ADD_TAG) && status != 2)
-            {
-                Serial.println("Add Tag");
-                status = 2;
-                uint16_t uid = mfrc522.uid.uidByte[0] << 8 | mfrc522.uid.uidByte[1];
-                accessControl.addEntry(uid, "Test");
-            }
-
-            if(isUID(PRINT_TAG) && status != 3)
-            {
-                Serial.println("Print Tag");
-                status = 3;
-                accessControl.printEntries();
-            }
+            printUID();   
         }
     }
 
@@ -116,6 +93,6 @@ public:
 
 private:
     MFRC522 & mfrc522;
-    AccessControl & accessControl;
-    uint8_t status = 0;
 };
+
+#endif /*__CARDREADER_H__*/
