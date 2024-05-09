bc5cfacc9bb7489986f830a7db75c6dc08ed17d2
jbotello <jlnbotello@gmail.com>
2024-05-09 16:11:34 +0200

    update: screen template

--- src/screens/TemplateScreen.h
+++ src/screens/TemplateScreen.h
@@ -1,6 +1,13 @@
 #ifndef __TEMPLATE_SCREEN_H__
 #define __TEMPLATE_SCREEN_H__
 
+/**
+ * Instructions:
+ *  - Repleace __TEMPLATE_SCREEN_H__ with __YOUR_SCREEN_NAME_H__
+ *  - Replace TemplateModel with your own data model. You can also include the model.
+ *  - Replace TemplateScreen with your own screen name
+ */
+
 #include "Screen.h"
 
 typedef uint8_t TemplateModel; // Create your own data model
